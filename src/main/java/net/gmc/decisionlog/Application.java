498cbaecd815a44e0a6485d16410255687b70f92
m.hatas <m.hatas@localhost>
2014-11-13 16:12:03 +0100

    sample data are generated when demomode is true

--- src/main/java/net/gmc/decisionlog/Application.java
+++ src/main/java/net/gmc/decisionlog/Application.java
@@ -1,11 +1,17 @@
 package net.gmc.decisionlog;
 
+import net.gmc.decisionlog.data.ElasticSearchStore;
+import net.gmc.decisionlog.model.Decision;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 
 @Configuration
@@ -19,6 +25,13 @@ public class Application extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) throws Exception {
+        run(args);
+    }
+
+    private static void run(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+
 }
--- src/main/java/net/gmc/decisionlog/data/ElasticSearchStore.java
+++ src/main/java/net/gmc/decisionlog/data/ElasticSearchStore.java
@@ -6,12 +6,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import net.gmc.decisionlog.model.Decision;
 import net.gmc.decisionlog.notification.publishing.DecisionEventPublisher;
 import org.apache.log4j.Logger;
+import org.elasticsearch.action.admin.cluster.state.ClusterStateResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.node.Node;
 import org.elasticsearch.search.SearchHit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -19,7 +22,9 @@ import javax.annotation.PreDestroy;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
@@ -35,15 +40,36 @@ public class ElasticSearchStore {
 
     private static Node node;
 
+    @Value("${demo}")
+    private boolean isDemo;
+
     @Autowired
     private DecisionEventPublisher publisher;
 
     @PostConstruct
-    private void init(){
+    private void init() throws InterruptedException {
         logger.info("Constructing and running ElasticSearch storage.");
         buildNode();
         createIndex();
+        waitUntilElasticSearchIsStarted();
+        listAllDecision();
+        createSampleData();
     }
+
+    private void waitUntilElasticSearchIsStarted() {
+        try {
+
+            ClusterStateResponse clusterState = getClusterState();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private ClusterStateResponse getClusterState() throws ExecutionException, InterruptedException {
+        return node.client().admin().cluster().prepareState().execute().get();
+    }
+
 
     private void buildNode() {
         node = nodeBuilder()
@@ -127,6 +153,28 @@ public class ElasticSearchStore {
     private void destroy(){
         logger.info("Gracefully shutting down ElasticSearch node.");
         node.close();
+    }
+
+    private void createSampleData() throws InterruptedException {
+        if (isDemo) {
+            Thread.sleep(5000);
+            if (listAllDecision().isEmpty()) {
+                Decision sampleDecision1 = new Decision();
+                sampleDecision1.setSubject("Long transactions");
+                sampleDecision1.setDate(new Date());
+                sampleDecision1.setReason("In Interactive Plus we have long transactions which causes performance problem and deadlocks.");
+                sampleDecision1.setConclusions("" +
+                        "1)\tVašek se podívá na zjednodušení volání SQL při schvalování. Všechny data by se měla načíst EAGER jedním SELECTEM pomocí JOINů přes ALIASY.\n" +
+                        "2)\tPetr mrkne na kaskády v databázi, které využijeme při mazání.\n" +
+                        "3)\tMartin H. se podívá na optimalizaci operací s DB, při vytváření snapshotů, taky aby se bloby kopírovaly přímo v DB a ne v Javě.\n" +
+                        "4)\tOndra koukne na optimalizaci při mazání draftů – budou pouze označeny jako deleted a z DB se smažou později.\n");
+                sampleDecision1.setAttendees(new String[]{"m.hatas", "m.jankovsky", "v.stolin", "h.steinmetz", "m.kosar"});
+                sampleDecision1.setTags(new String[]{"#performance", "#transactions", "#database", "#deadlock", "#peak"});
+
+                saveDecision(sampleDecision1);
+            }
+        }
+
     }
 
 }
