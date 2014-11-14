85ea407159c651524a13bf1b405950c54ddbdd9d
m.hatas <m.hatas@localhost>
2014-11-14 11:19:07 +0100

    hlaska

--- src/main/java/net/gmc/decisionlog/model/Decision.java
+++ src/main/java/net/gmc/decisionlog/model/Decision.java
@@ -149,6 +149,6 @@ public class Decision {
     }
 
     public String getSkypeMessage() {
-        return String.format("New decision record about '%s' has been saved. You can visit it here. http://gmc-decisionlog/%s", subject, id);
+        return String.format("New decision record about '%s' has been saved. You can check it here. http://gmc-decisionlog/%s", subject, id);
     }
 }
